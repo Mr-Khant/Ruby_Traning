17a610a0716edc0eda10e2e004dab6323b2fa524
Mr-Khant <103758712+Mr-Khant@users.noreply.github.com>
2022-10-03 14:35:38 +0630

    changes for tutorial06

--- tutorial06.rb
+++ tutorial06.rb
@@ -4,14 +4,12 @@ name = gets.chomp()
 puts "What do you write in the file?? :"
 data = gets.chomp()
 
-
 # create file
 file = File.open(name + ".txt" , "w")
-    file.write(data)
+  file.write(data)
 file.close()
 
 # opening file
-
 file = File.open(name + ".txt" , "r")
-    puts file.read()
+  puts file.read()
 file.close
