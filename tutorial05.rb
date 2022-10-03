2893a9342b6dd994f8fc48d07b6b424997791937
Mr-Khant <103758712+Mr-Khant@users.noreply.github.com>
2022-10-03 14:29:29 +0630

    changes for tutorial05

--- tutorial05.rb
+++ tutorial05.rb
@@ -1,26 +1,19 @@
 date = Time.now
-tdy = date.strftime("%A")
+today = date.strftime("%A")
 
-if tdy == "Monday"
-    puts "Wednesday , Thursday , Friday , Saturday , Sunday"
-
-    elsif tdy == "Tuesday"
-        puts "Thursday , Friday , Saturday , Sunday , Monday"
-        
-    elsif tdy == "Wednesday"
-        puts "Friday , Saturday , Sunday , Monday , Tuesday "
-
-    elsif tdy == "Thursday"
-        puts "Saturday , Sunday , Monday , Tuesday , Wednesday "
-
-    elsif tdy == "Friday"
-        puts "Sunday , Monday , Tuesday , Wednesday , Thursday "
-
-    elsif tdy == "Saturday"
-        puts "Monday , Tuesday , Wednesday , Thursday , Friday "
-
-    elsif tdy == "Sunday"
-        puts "Tuesday , Wednesday , Thursday , Friday , Saturday"
-
+if today === "Monday"
+  puts "Wednesday , Thursday , Friday , Saturday , Sunday"
+  elsif today === "Tuesday"
+    puts "Thursday , Friday , Saturday , Sunday , Monday"     
+  elsif today === "Wednesday"
+    puts "Friday , Saturday , Sunday , Monday , Tuesday "
+  elsif today === "Thursday"
+    puts "Saturday , Sunday , Monday , Tuesday , Wednesday "
+  elsif today === "Friday"
+    puts "Sunday , Monday , Tuesday , Wednesday , Thursday "
+  elsif today === "Saturday"
+    puts "Monday , Tuesday , Wednesday , Thursday , Friday "
+  elsif today === "Sunday"
+    puts "Tuesday , Wednesday , Thursday , Friday , Saturday"
 end
 
